1933d49f226c509a84dc2ca1c1f9d8ee96a81a27
Terminator1601 <rishisharma4201@gmail.com>
2023-12-13 16:32:48 +0530

    study in india added

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,10 +1,9 @@
-// components/ImageDropdownLayout.js
 import React from "react";
-// import styles from "@/components/Header/Header.css"
+// import styles from "@/components/styles/MovingText.module.css"
 
 const Header = () => {
   return (
-    <div className="flex">
+    <div className="flex" style={{textAlign:"center"}}>
       {/* Image Section */}
       <div className="flex-1">
         <img
@@ -14,40 +13,51 @@ const Header = () => {
         />
       </div>
 
-      {/* Dropdown Section */}
       {/* <div className="flex-1 p-4 flex flex-col"> */}
       <div className="text-lg flex">
         <div className="mb-4 text-lg">
-          <a href="/" className="" style={{textDecoration:"none"}}>
+          <a href="/" className="" style={{ textDecoration: "none" }}>
             <label className="block text-lg font-medium px-6 py-3">Home</label>
           </a>
         </div>
       </div>
       <div className="text-lg flex">
         <div className="mb-4 text-lg">
-          <a href="/" className="" style={{textDecoration:"none"}}>
-            <label className="block text-lg font-medium px-6 py-3">Study In India</label>
+          <a
+            href="/StudyInIndia"
+            className=""
+            style={{ textDecoration: "none" }}
+          >
+            <label className="block text-lg font-medium px-6 py-3">
+              Study In India
+            </label>
           </a>
         </div>
       </div>
       <div className="text-lg flex">
         <div className="mb-4 text-lg">
-          <a href="/" className="" style={{textDecoration:"none"}}>
-            <label className="block text-lg font-medium px-6 py-3">Study In Abroad</label>
+          <a href="/" className="" style={{ textDecoration: "none" }}>
+            <label className="block text-lg font-medium px-6 py-3">
+              Study In Abroad
+            </label>
           </a>
         </div>
       </div>
       <div className="text-lg flex">
         <div className="mb-4 text-lg">
-          <a href="/" className="" style={{textDecoration:"none"}}>
-            <label className="block text-lg font-medium px-6 py-3">Predict Your College</label>
+          <a href="/" className="" style={{ textDecoration: "none" }}>
+            <label className="block text-lg font-medium px-6 py-3">
+              Predict Your College
+            </label>
           </a>
         </div>
       </div>
       <div className="text-lg flex">
         <div className="mb-4 text-lg">
-          <a href="/" className="" style={{textDecoration:"none"}}>
-            <label className="block text-lg font-medium px-6 py-3">About Us</label>
+          <a href="/" className="" style={{ textDecoration: "none" }}>
+            <label className="block text-lg font-medium px-6 py-3">
+              About Us
+            </label>
           </a>
         </div>
       </div>
---
+++ pages/StudyInIndia.js
@@ -0,0 +1,17 @@
+import Header from '@/components/Header/Header'
+import React from 'react'
+import  '@/components/styles/Header.css'
+import CoursesSection from '@/components/CourseSection'
+import MovingText from '@/components/Header/MovingText'
+
+const StudyInIndia = () => {
+  return (
+    <>
+    <Header/>
+    <MovingText/>
+    <CoursesSection/>
+    </>
+  )
+}
+
+export default StudyInIndia
